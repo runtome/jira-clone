f78f74758317623b65bf98706ddc1da4b117ac70
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-16 23:21:15 +0700

    change page to button (test)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // Include pages folder (tailwind will work)
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // Include components folder
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Include app folder 
   ],
   theme: {
   	extend: {
