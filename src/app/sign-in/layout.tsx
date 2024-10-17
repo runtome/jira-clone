81dcc9db69907a45fc0ee1ab296187645b452467
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-17 12:43:49 +0700

    Making sign in Layout and page (concept)

---
+++ src/app/sign-in/layout.tsx
@@ -0,0 +1,15 @@
+interface SignInLayoutProps {
+  children: React.ReactNode;
+}
+const SignInLayout = ({children} : SignInLayoutProps) => {
+  return (
+    <div className="flex flex-col">
+      <nav className="bg-red-500 h-10"> 
+        <p>Navbar</p>
+      </nav>
+      {children}
+    </div>
+  )
+}
+
+export default SignInLayout;
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import exp from "node:constants"
+
+const SignInPage = () => {
+  return (
+    <div>
+      Sign In Page
+    </div>
+  )
+}
+
+export default SignInPage
