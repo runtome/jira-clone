7a00b6adf69839d5efee6a320bd4ec99c4dca1ac
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-17 11:21:01 +0700

    Change  Button component sytle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,29 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <Button variant="outline" size='sm'>Button</Button>
-      <p className="text-red-500 font-semibold">
-        Hello, Next.js!
-      </p>
+      <Button>
+        Primary
+      </Button>
+      <Button variant="destructive">
+        Destructive
+      </Button>
+      <Button variant="outline">
+        Outline
+      </Button>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+      <Button variant="ghost">
+        Ghost
+      </Button>
+      <Button variant="muted">
+        Muted
+      </Button>
+      <Button variant="terimary">
+        Terimary
+      </Button>
+
+
     </div>
 
   )
