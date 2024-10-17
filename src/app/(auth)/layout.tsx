429e9c2ca9e598bad2fb2c2930c2e6876ac93279
SUPHOT NARAPONG <suphot.n@gmail.com>
2024-10-17 13:21:08 +0700

    make logo simple nav (example)

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,15 +5,21 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div>
-      <Image
-        src="/logo.svg"
-        height={100}
-        width={200}
-        alt="logo"
-        />
-      {children}
-    </div>
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={152}
+              height={56}
+            />
+          </div>
+        </nav>
+        {children}
+      </div>
+    </main>
   )
 }
 
